572f4700f0c07e010593ec825e190d50b58497ef
Jeff Simons Decena <jeff.decena@yahoo.com>
2015-07-04 16:19:28 +0800

    Refactoring front end

--- app/views/admin/login.blade.php
+++ app/views/admin/login.blade.php
@@ -1,40 +1,17 @@
 @extends('admin.tpl.login')
 
-@section('meta-title')
-Log In
-@stop
+@section('meta-title') Log In @stop
 
 @section('body')
     <div class="form-box" id="login-box">
         <div class="header">Log In</div>
         
         {{ Form::open(array('url' => Request::path())) }}
-        
-            <div class="body bg-gray">
-                {{-- CHECK IF THERE IS A SESSION MESSAGE FOR FAILED ATTEMPT --}}
-                @if($errors->all())
-                    <ul class="list-unstyled">
-                        @foreach($errors->all() as $error)
-                            <li class="alert alert-danger">
-                                <button type="button" class="close" data-dismiss="alert">&times;</button>
-                                {{ $error }}
-                            </li>
-                        @endforeach
-                    </ul>
-                @endif
 
-                @if(Session::get('success'))
-                    <p class="alert alert-success">
-                        <button type="button" class="close" data-dismiss="alert">&times;</button>
-                        {{ Session::get('success') }}
-                    </p>
-                @elseif(Session::get('error'))
-                    <p class="alert alert-danger">
-                        <button type="button" class="close" data-dismiss="alert">&times;</button>
-                        {{ Session::get('error') }}
-                    </p>
-                @endif
+            <div class="body bg-gray">
+                @include('messages')
             </div>
+
             <div class="body bg-gray">
                 <div class="form-group">
                     <input type="text" name="email" class="form-control" placeholder="Email" value="{{ Input::old('email') }}"/>
