a66ed97903c129bec487e5b0cbf1e946b510f2e1
Jeff Simons Decena <jeff.decena@yahoo.com>
2015-07-11 09:36:12 +0800

    Added a category page.

--- app/views/front/clean/category.blade.php
+++ app/views/front/clean/category.blade.php
@@ -1,56 +1,38 @@
-@extends('front.marketing.tpl.main')
-
-@section('meta-title')
-{{ ucwords(Request::segment(1)) }}
-@stop
+@extends('front.'.$theme.'.tpl.main')
 
 @section('body')
-	<div class="col-md-8 wrap">
-		<h1>{{ $category->category }}</h1> <hr>
-	    @if(isset($posts) && !empty($posts) && ! $posts->isEmpty())
-		    @foreach( $posts as $post )
-			    <article class="row">
-			        <div class="span3 pull-left">
-			        	<a href="{{ URL::to($post->slug) }}">
-				            @if( isset($post->img_src))			            	
-				            	<img src="{{ asset("uploads/$post->img_src") }}" alt="{{ $post->title }}" width="250" height="150" class="img-responsive">			            	
-				            @else
-								<img src="http://placehold.it/250x150" alt="no-featured-image" class="img-responsive">
-				            @endif
-			            </a>
-			        </div>
-			        <div class="span4 pull-right">
-			            <h3><a href="{{ URL::to($post->slug) }}">{{ $post->title }}</a></h3>
-			            <p><span class="fa fa-calendar"></span> Posted on {{ date('F d Y', strtotime($post->created_at)) }}</p>
-			            <div class="content">{{ str_limit($post->content, 250, ' ...') }}</div>
-			            <a class="btn btn-primary read-more" href="{{ URL::to($post->slug) }}">Read More <span class="fa fa-chevron-right"></span></a>
-			        </div>
-			    </article>
-		    @endforeach
-		    {{ $posts->links() }}
-		@else
-			<p class="alert alert-danger">Sorry, not posts to show.</p>
-		@endif
-	</div>
-	<aside class="col-md-4">
-		<div class="sidebar archives">
-			<h2>Search</h2>
-			{{ Form::open(array('url' => 'search', 'role'=>'form', 'class' => 'form-horizontal', 'method' => 'get')) }}
-			<div class="form-group">
-				<div class="input-group">
-					<input type="text" class="form-control" name="term" value="" placeholder="Search now">
-					<div class="input-group-addon"><i id="btn_search" class="glyphicon glyphicon-search"></i></div>
-				</div>
-			</div>					
-			{{ Form::close() }}
-		</div>				
-		<div class="sidebar archives">
-			<h2>Archives</h2>
-			<ul>
-				<li><a href="#">January</a></li>
-				<li><a href="#">February</a></li>
-				<li><a href="#">March</a></li>
-			</ul>
-		</div>
-	</aside>		
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header" style="background-image: url('{{URL::to('themes/front/clean/img/post-bg.jpg')}}')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="site-heading">
+                        <h1>{{$category->name}}</h1>
+                        <hr class="small">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+	@if(!$records->isEmpty())
+		@foreach($records as $record)
+		    <!-- Post Content -->
+		    <article>
+		        <div class="container">
+		            <div class="row">
+		                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+		                    <h1><a href="{{URL::route('post.show', $record['post']->slug)}}">{{$record['post']->title}}</a></h1>
+		                    <p class="post-meta">Posted by <a href="#">{{$record['author']->firstname}}</a> on {{date('M d Y', strtotime($record['post']->created_at))}}</p>
+		                    {{$record['post']->content}}
+		                </div>
+		            </div>
+		        </div>
+		    </article>
+		    <hr>
+	    @endforeach
+	    <article><div class="container"><div class="row"><div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">{{$records->links()}}</div></div></div></article>
+	@else
+		<p class="alert alert-warning">No post to show.</p>
+	@endif
 @stop
